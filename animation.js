893ea315a7126f0801cd5f036721499f9066fb87
Farrell Christian <131789889+farrellchristian@users.noreply.github.com>
2024-12-06 10:47:50 +0700

    Create animation.js
    menambahkan animation.js

---
+++ animation.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Halaman sudah siap! Tambahkan animasi atau efek di sini.');
+    // Anda dapat menambahkan efek partikel atau animasi lain sesuai kebutuhan.
+});
